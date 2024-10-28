a2c96d3b10b42593ce62c1634b982cd5252eb4f0
Ray1457-2 <rishitnarang222@outlook.com>
2024-10-28 12:36:01 +0530

    user mixin

--- Tool/models.py
+++ Tool/models.py
@@ -1,11 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from Tool import db, login_manager
+from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
